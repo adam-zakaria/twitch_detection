3af5200902f42c08be1f725aea6f2e8ed08464a6
Adam Zakaria <adam.p.zakaria@gmail.com>
2025-01-09 03:20:37 -0500

    Add uuid

--- download_twitch_streams.py
+++ download_twitch_streams.py
@@ -1,28 +1,33 @@
 import subprocess; import utils.utils as utils
 
 # If streamer is live, download stream --------------------
-#for streamer in ['Luciid_TW', 'ItsTheLastShot', 'SpartanTheDogg', 'SnakeBite', 'aPG']:
-for streamer in ['LuciidTW']:
+
+for streamer in ['aPG', 'Gunplexion']:
   output_dir = f'twitch_streams/{streamer}';
   # Is streamer live?
   if "The channel is not currently live" in subprocess.run(['yt-dlp', '--get-url', '-S', 'vcodec:h265,acodec:aac', f'https://www.twitch.tv/{streamer}','-o', f'{output_dir}/stream.%(ext)s'], text=True, capture_output=True).stderr: # text = true converts the stdout and stderr streams to Python str objects (text).
-  #if "The channel is not currently live" in subprocess.run(['yt-dlp', '--get-url', '-S', 'vcodec:h265,acodec:aac', f'https://www.twitch.tv/{streamer}','-o', f'{output_dir}/stream.%(ext)s'], text=True).stderr: # text = true converts the stdout and stderr streams to Python str objects (text).
     print(f"{streamer} is not currently live.")
   else:
     # if live, download stream
-    utils.rm_mkdir(output_dir)
+    utils.mkdir(output_dir)
     print(f"Downloading stream for {streamer}...")
-    subprocess.run(['yt-dlp', '-S', 'vcodec:h265,acodec:aac', f'https://www.twitch.tv/{streamer}','-o', f'{output_dir}/stream.%(ext)s'])
+    import uuid
+    subprocess.Popen(['yt-dlp', '-S', 'vcodec:h265,acodec:aac', f'https://www.twitch.tv/{streamer}','-o', f'{output_dir}/{uuid.uuid4().hex}.%(ext)s'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)  # Set a new process group)
 
-  # output twitch stream to twitch_streams/{user}/stream.(%ext)
-  # capture stdout: if the message includes "The channel is not currently live", the n
-  # ERROR: [twitch:stream] luciid_t: The channel is not currently live
-  # Run the command and capture the output
-
+while(True):
+  pass
 
 """
-# the message we get if we don't use any special flag and the user if not streaming. we can either check this output or use the flag and see if there's no output?
-[twitch:stream] Extracting URL: https://www.twitch.tv/LuciidTW
-[twitch:stream] luciidtw: Downloading stream GraphQL
-ERROR: [twitch:stream] luciidtw: The channel is not currently live
+# Notes ####################
+# Problems . . .
+One problem right now is if the streams start and stop, which is common. The existing will file be deleted. Also, when we restart the script, new download streams start. At least, ps | grep python shows the yt-dlps.
+
+If we close the download_twitch_streams.py it stops running part if we're doing watchman...
+
+Maybe this actually isn't a problem, just watchman quirks
+
+Maybe just give the file names uuids? and remove the folders once the detections are run.
+
+# Streamer list
+['Luciid_TW', 'ItsTheLastShot', 'SpartanTheDogg', 'SnakeBite', 'aPG']:
 """
