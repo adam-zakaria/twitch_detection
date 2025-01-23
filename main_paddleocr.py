1bfa958e23dbb38578f8df7339ee00244bf7d07e
Adam Z <adam.p.zakaria@gmail.com>
2025-01-23 17:05:42 -0500

    Add main_paddleocr

--- main_paddleocr.py
+++ main_paddleocr.py
@@ -1,7 +1,53 @@
-from paddleocr import PaddleOCR, draw_ocr; from PIL import Image; import os; import cv2; import utils.utils as utils; from itertools import pairwise; import subprocess
+from paddleocr import PaddleOCR, draw_ocr; from PIL import Image; import os; import cv2; import utils.utils as utils; from itertools import pairwise; import subprocess; import cliptu.clip as clip
 
 # Initialize the PaddleOCR model; uses English language and angle classification
 ocr = PaddleOCR(use_angle_cls=True, lang='en', show_log=False, use_gpu=True)  # Load OCR model once during initialization
+
+
+# Function to process a video and extract OCR text from each frame
+def detect(video_path, output_dir):
+  os.makedirs(output_dir, exist_ok=True)  # Ensure the output directory exists or create it
+
+  # Open the output file to write detection times
+  detections_file = os.path.join(output_dir, 'dk_detections.txt')
+  with open(detections_file, 'w') as f:
+
+    # Iterate through frames using the get_frames generator
+    for frame_index, (frame, fps) in enumerate(utils.get_frames(video_path), start=1):
+
+      # Validate cropping dimensions
+      # For some reason this is not working on M2, but I'm PRETTY sure it wasn't an issue on GPU, so ignore for now. I just need the detections file to do the rest of the pipeline.
+      crop_x, crop_y, crop_w, crop_h = 529, 441, 266, 131  # Define crop dimensions
+      frame_h, frame_w = frame.shape[:2]  # Get frame dimensions
+      if crop_x + crop_w > frame_w or crop_y + crop_h > frame_h:
+        print(f"Skipping frame {frame_index}: Crop dimensions out of bounds")
+        continue
+      else:
+        print(f"Frame dimensions: {crop_x, crop_y, crop_w, crop_h}")
+      # Crop the frame
+      frame = frame[crop_y:crop_y + crop_h, crop_x:crop_x + crop_w]  # Perform cropping
+
+      # Perform OCR on the saved frame image
+      result = ocr.ocr(frame, cls=True)  # Extract text and bounding boxes from the frame
+      result = result[0]
+
+      # Print OCR results for the current frame
+      print(f"Frame {frame_index}")
+      for line in result:
+        text = line[1][0]  # Extract the recognized text
+
+        #confidence = line[1][1]  # Extract the confidence score
+        #print(f"Text: {text}, Confidence: {confidence}")
+
+        # Check for the presence of the word 'Dou' in the recognized text
+        if 'Dou' in text:
+          print(f'****************************')
+          print(f"Match found: {text}")
+          print(f'****************************')
+          
+          # Calculate the timestamp for the detection
+          detection_time = frame_index / fps  # Frame index divided by FPS gives the time in seconds
+          utils.wa(f"{detection_time:.2f}\n", detections_file)  # Write the detection time to the file
 
 # Function to process a video and extract OCR text from each frame
 def detect_paddleocr(video_path, output_dir):
@@ -18,6 +64,7 @@ def detect_paddleocr(video_path, output_dir):
     cv2.imwrite(frame_path, frame)  # Write the frame as an image file
 
     # Perform OCR on the saved frame image
+    # result = ocr.ocr(frame, cls=True)  # Extract text and bounding boxes from the frame
     result = ocr.ocr(frame_path, cls=True)  # Extract text and bounding boxes from the frame
     result=result[0]
 
@@ -61,7 +108,6 @@ def filter(detections_path):
     else:
        return mk_ss
     
-import cliptu.clip as clip
 def extract(input_video_path, output_video_folder, detections: float):
     """
     Extracts video clips based on detection times.
@@ -97,7 +143,7 @@ def concat(input_clips_path, output_folder):
 # Main entry point of the script
 if __name__ == "__main__":
   # Process a sample video and save results to the specified output directory
-  #detect_paddleocr('test/videos/1s_dk.mp4', 'test/ocr_results')
+  detect_paddleocr('test/videos/1s_dk.mp4', 'detect')
   detections = filter('/Users/azakaria/Code/twitch_detections/paddleocr_output/dk_detections_original.txt')
   print(detections)
   extract('/Users/azakaria/Code/twitch_detections/test/videos/4m_dk.mp4','extract', detections)
