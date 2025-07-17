78597545291df529bd9210e3b4ccd1ff7a7b3b4e
Adam Zakaria <adam.p.zakaria@gmail.com>
2025-07-16 23:57:00 -0400

    Add pipeline.process() to schedule

--- twitch_detections/main_schedule_infinity.py
+++ twitch_detections/main_schedule_infinity.py
@@ -3,39 +3,82 @@ import os
 import signal
 import subprocess
 import time
+import happy_utils as utils
+import pipeline
+import path
+import glob
+
+"""
+Instead of start and killing processes, just process videos which are after the time and remove the ones before. Or some other kind of file maintenance. Not sure this is really possible, because new streams would still need to be created...
+"""
+"""
+Currently:
+
+* Exploring cases:
+  * A streamer is not streaming - if retry is configured, os.killpg does not work. But it does not need to work - the point of killing a download is so that the stream can be processed and redownloaded with another filename. 
+  * A streamer is streaming - does try catch work?
+ 
+Logging would be nice.
+"""
+
 
 def start_procs(streamers, procs):
   # Start downloads
   # clear out any old entries from process list
   procs.clear()
   for streamer in streamers:
-    proc = subprocess.Popen(["yt-dlp", "--cookies", "cookies.txt", "--wait-for-video", "600", "-S", f'vcodec:h265,acodec:aac', "--no-part", f"https://www.twitch.tv/{streamer}"], preexec_fn=os.setsid)
+    # Create output folder
+    output_folder = f'output/{streamer}/stream/'
+    utils.mkdir(output_folder)
+    # download and output to output/{streamer}/stream/{streamer.mp4}
+    #proc = subprocess.Popen(["yt-dlp", "--cookies", "cookies.txt", "--wait-for-video", "600", "-S", f'vcodec:h265,acodec:aac', "--no-part", f"https://www.twitch.tv/{streamer}", '-o',
+    proc = subprocess.Popen(["yt-dlp", "--cookies", "cookies.txt", "-S", f'vcodec:h265,acodec:aac', "--no-part", f"https://www.twitch.tv/{streamer}", '-o',
+    f'{output_folder}/{streamer}-{utils.ts()}.mp4'], preexec_fn=os.setsid)
     procs.append(proc)
 
 def kill_procs(procs):
   # Kill processes
+  print('Kill processes')
   for proc in procs:
     os.killpg(proc.pid, signal.SIGINT)
+    # os.killpg(proc.pid, signal.SIGKILL)
     proc.wait() # Best practice to fully cleanup subprocess
 
+def process_streams():
+  # start processing once the streams are killed.
+  for stream_path in glob.glob(f'output/**/stream/*.mp4'):
+    pipeline.process(stream_path)
+
 # Start downloads now
-streamers = ['Luciid_TW', 'Formal', 'frosty']
+#streamers = ['gunplexion', 'gamesager']
+streamers = ['pzzznguin'] 
 procs = []
 start_procs(streamers, procs)
 
+# generate times for testing
+from datetime import datetime, timedelta
+now = datetime.now()
+kill_time = (now + timedelta(minutes=1)).strftime("%H:%M")
+process_time = (now + timedelta(minutes=2)).strftime("%H:%M")
+
 # Kill downloads at 2PM everyday
-schedule.every().day.at("15:08").do(
+schedule.every().day.at(kill_time).do(
   kill_procs,
   procs
 )
 
-# Start downloads at 201PM everyday
-schedule.every().day.at("15:10").do(
-  start_procs,
-  streamers,
-  procs
+schedule.every().day.at(process_time).do(
+  process_streams
 )
 
+
+## Start downloads at 201PM everyday
+#schedule.every().day.at("22:26").do(
+#  start_procs,
+#  streamers,
+#  procs
+#)
+#
 while True:
   schedule.run_pending()
   time.sleep(1)
--- twitch_detections/pipeline.py
+++ twitch_detections/pipeline.py
@@ -6,35 +6,53 @@ import sys
 from pathlib import Path
 import happy_utils as utils
 import time
+import glob
 
-# Initialize paths
-roi_frames_dir = './output'
-template_path = '/Users/azakaria/Code/twitch_detections/twitch_detections/test/frame/double_kill_tighter.png'
-stream_path = 'lucid_3m.mp4' # dk at ~2:16
+def process(stream_path=''):
+  """
+  * Reframe
+  * Template match
+  * Extract clips
+  * Concatenate clips
+  """
+  try:
+    # Initialize paths
+    roi_frames_dir = './output'
+    template_path = 'test/frame/double_kill_tighter.png'
 
-# Reframe video to an ROI, only take every 60th frame. This is very slow. There are options for speeding up, with ffmpeg maybe being 10x faster? https://chatgpt.com/share/6875d629-1a94-8013-9af4-001b34edfea4 Could be a pain. Leave it unless it's an issue :)
-print('Reframing video to an ROI')
-start_time = time.time()
-timestamps_and_frames = cliptu_utils.reframe_video_mem(input_path=stream_path, x=710, y=479, w=200, h=200, every_nth_frame=60)
-end_time = time.time()
-print(f'Reframing video to an ROI took {end_time - start_time} seconds')
+    # Reframe video to an ROI, only take every 60th frame. This is very slow. There are options for speeding up, with ffmpeg maybe being 10x faster? https://chatgpt.com/share/6875d629-1a94-8013-9af4-001b34edfea4 Could be a pain. Leave it unless it's an issue :)
+    print('Reframing video to an ROI')
+    start_time = time.time()
+    timestamps_and_frames = cliptu_utils.reframe_video_mem(input_path=stream_path, x=710, y=479, w=200, h=200, every_nth_frame=60)
+    end_time = time.time()
+    print(f'Reframing video to an ROI took {end_time - start_time} seconds')
 
-# Template match each frame
-template_match_dir = './template_match'
-print('Template matching each frame')
-start_time = time.time()
-match_timestamps = cliptu_utils.template_match_folder(timestamps_and_frames=timestamps_and_frames, output_folder_path=template_match_dir, template_image_path=template_path, log_file_path='./log.txt', threshold=.8)
-end_time = time.time()
-print(f'Template matching each frame took {end_time - start_time} seconds')
+    # Template match each frame
+    template_match_dir = './template_match'
+    print('Template matching each frame')
+    start_time = time.time()
+    match_timestamps = cliptu_utils.template_match_folder(timestamps_and_frames=timestamps_and_frames, output_folder_path=template_match_dir, template_image_path=template_path, log_file_path='./log.txt', threshold=.8)
+    end_time = time.time()
+    print(f'Template matching each frame took {end_time - start_time} seconds')
 
-# Expecting template matches in template_match_dir
-filtered_timestamps = cliptu_utils.filter_timestamps(match_timestamps)
+    # Expecting template matches in template_match_dir
+    filtered_timestamps = cliptu_utils.filter_timestamps(match_timestamps)
 
-# Extract clips
-# Would prefer 
-paths = []
-output_dir = './clips'
-utils.mkdir(output_dir)
-for timestamp in filtered_timestamps:
-  paths.append(clip.extract_clip(stream_path, f'{output_dir}/{timestamp}.mp4', timestamp-6, timestamp + 3))
-clip.concat(paths)
+    # Extract clips
+    paths = []
+    output_dir = './clips'
+    utils.mkdir(output_dir)
+    for timestamp in filtered_timestamps:
+      paths.append(clip.extract_clip(stream_path, f'{output_dir}/{timestamp}.mp4', timestamp-6, timestamp + 3))
+    clip.concat(paths)
+
+    # remove processed streams
+    for stream_path in glob.glob(f'output/**/stream/*.mp4'):
+      utils.rm(stream_path)
+  except:
+    print('Exception in process')
+
+
+if __name__ == "__main__":
+  stream_path = 'lucid_3m.mp4' # dk at ~2:16
+  process(stream_path)
